b586f1ccece50ea1507269098ada0431fda9713e
jtanquil <jose.tanquiilut@gmail.com>
2025-03-20 08:59:33 +0800

    ex 4.12

--- ch4/ex12.c
+++ ch4/ex12.c
@@ -6,14 +6,20 @@ iota; that is, convert an integer into a string by calling a recursive routine. 
   if it's -2^(wordsize - 1), then return itoa(n + 1) with the ones digit + 1,
   otherwise,
   if it's < 0 note this and switch its sign to positive
-  if it's < 10 add the ones digit to the front of the string
-  otherwise, shift the string elements by 1 and invoke itoa(n / 10)
-  if the noted sign is negative, then shift the string elements and add a negative
+
+  if n >= 10, then call itoa(n / 10)
+
+  otherwise, set the last element of s to n % 10 
+  then add '\0' to the end (this is the new last element) 
+  if n < 0, shift the elements of s and add a -
 */
 
 #include <stdio.h>
 #include <limits.h>
 
+/* shifts the characters of s one space forward
+   assumes that the length of s is >= 1 more than the number of characters in s
+*/
 void shift(char s[]) {
   int i;
 
@@ -42,15 +48,36 @@ void itoa(int n, char s[]) {
     sign = (n < 0) ? -1 : 1;
     n = (sign < 0) ? -n : n;
 
-    if (n < 10) {
+    if (n >= 10) {
+      itoa(n / 10, s);
+    }
+
+    for(i = 0; s[i] >= '0' && s[i] <= '9'; i++) {
       ;
+    }
+
+    s[i++] = (n % 10) + '0';
+    s[i] = '\0';
+
+    if (sign < 0) {
+      shift(s);
+      s[0] = '-';
     }
   }
 }
 
 void main() {
-  char s[33] = "                                ";
+  char s[30], t[30], u[30], v[30];
 
   itoa(INT_MIN, s);
   printf("%s\n", s);
+
+  itoa(INT_MAX, t);
+  printf("%s\n", t);
+
+  itoa(0, u);
+  printf("%s\n", u);
+
+  itoa(-4416, v);
+  printf("%s\n", v);
 }
